f5e435706e9302c8918d12f24aeac024965ab547
duy <nmnduy@gmail.com>
2025-10-25 02:58:43 -0500

    M run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 set -xe
-make &&  CLAUDE_THEME=./colorschemes/kitty-default.conf ./build/claude
+make &&  CLAUDE_THEME=./colorschemes/kitty-default.conf \
+  OPENAI_API_KEY=$OPENROUTER_API_KEY \
+  OPENAI_API_BASE=https://openrouter.ai/api \
+  OPENAI_MODEL=z-ai/glm-4.6 \
+  ./build/claude
