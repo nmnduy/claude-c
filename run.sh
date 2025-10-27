82c03e4978f1c2bdb74b0272a92fe2ce68d77cf2
duy <nmnduy@gmail.com>
2025-10-27 05:29:58 -0500

    M run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 set -xe
 make && CLAUDE_THEME=${CLAUDE_THEME:-dracula} \
   ./build/claude-c
-  # OPENAI_API_BASE=${OPENAI_API_BASE:-https://openai.ai/api} \
-  # OPENAI_MODEL=${OPENAI_MODEL:-z-ai/glm-4.6} \
+
+# examples config:
+# export OPENAI_API_KEY=$OPENROUTER_API_KEY \
+# OPENAI_API_BASE=${OPENAI_API_BASE:-https://openai.ai/api} \
+# OPENAI_MODEL=${OPENAI_MODEL:-z-ai/glm-4.6} \
