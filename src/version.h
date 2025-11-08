13346e31f5414327a4cc07e3d5c18d38421e5e95
duy <nmnduy@gmail.com>
2025-11-07 23:05:13 -0600

    chore: bump version to 0.1.2

--- src/version.h
+++ src/version.h
@@ -9,20 +9,20 @@
 #define VERSION_H
 
 // Version string (e.g., "0.0.2", "1.0.0", "1.2.3-beta.1")
-#define CLAUDE_C_VERSION "0.1.1"
+#define CLAUDE_C_VERSION "0.1.2"
 
 // Version components for programmatic use
 #define CLAUDE_C_VERSION_MAJOR 0
 #define CLAUDE_C_VERSION_MINOR 1
-#define CLAUDE_C_VERSION_PATCH 1
+#define CLAUDE_C_VERSION_PATCH 2
 
 // Version as numeric value for comparisons (e.g., 0x000002)
-#define CLAUDE_C_VERSION_NUMBER 0x000101
+#define CLAUDE_C_VERSION_NUMBER 0x000102
 
 // Build timestamp (automatically generated)
 #define CLAUDE_C_BUILD_TIMESTAMP "2025-11-07"
 
 // Full version string with build info
-#define CLAUDE_C_VERSION_FULL "0.1.1 (built 2025-11-07)"
+#define CLAUDE_C_VERSION_FULL "0.1.2 (built 2025-11-07)"
 
 #endif // VERSION_H
